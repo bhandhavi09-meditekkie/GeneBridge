54ffefbe01316bebfe0e3a34bdbdeaaae7eeb138
bhandhavi09-meditekkie <bhandhavikiran.baladari@gmail.com>
2026-04-28 21:25:57 +0530

    Refactor App class to GeneBridgeApplication with Spring Boot

--- genebridge-backend/src/main/java/com/genebridge/App.java
+++ genebridge-backend/src/main/java/com/genebridge/App.java
@@ -1,13 +1,6 @@
-package com.genebridge;
-
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+@SpringBootApplication
+public class GeneBridgeApplication {
+   public static void main(String[] args){
+      SpringApplication.run(GeneBridgeApplication.class,args);
+   }
 }
